25c66ffd579e95255eff4fbaa6d2bd3a3b64458f
kevuo <kevgiso@hotmail.com>
2015-09-27 17:32:03 -0600

    Se crea el objeto Metadata el cual permite encapsular el contenido de la misma.

---
+++ src/Shared/Structures/Metadata.java
@@ -0,0 +1,33 @@
+package Shared.Structures;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Clase encargada de encapsular la metadata.
+ *
+ * @author Kevin
+ */
+public class Metadata implements Serializable {
+
+    private ArrayList<ArrayList<ArrayList<String>>> metadata;
+
+    public Metadata(ArrayList<ArrayList<ArrayList<String>>> metadata) {
+        this.metadata = metadata;
+    }
+
+    /**
+     * @return the metadata
+     */
+    public ArrayList<ArrayList<ArrayList<String>>> getMetadata() {
+        return metadata;
+    }
+
+    /**
+     * @param metadata the metadata to set
+     */
+    public void setMetadata(ArrayList<ArrayList<ArrayList<String>>> metadata) {
+        this.metadata = metadata;
+    }
+
+}
