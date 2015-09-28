375109360337bd877ddcebcb825a828bad81f1c3
majesco <nicolas.j2007@gmail.com>
2015-09-27 18:33:49 -0600

    Mighty Updated

--- src/DatabaseRuntimeProcessor/MightyMain.java
+++ src/DatabaseRuntimeProcessor/MightyMain.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
  * @author nicolasjimenez
  */
 public class MightyMain {
+    
+    private String databaseName;
 
     /**
      * Ejecuta main
@@ -35,28 +37,37 @@ public class MightyMain {
         }
     }
 
-    private void processer(ArrayList<String> instruccion, String databaseName) {
+    private void processer(ArrayList<String> instruccion) {
 
         String instruction0 = instruccion.get(0);
 
         switch (instruction0) {
+            
+            
 
             case "create":
+                
+                if (instruccion.get(1).equals("index")) {
+                    
+                    String nombreIndice = instruccion.get(2);
+                    String tabla = instruccion.get(4);
+                    String columna = instruccion.get(6);
+                }
 
-                if (instruccion.get(1).equals("database")) {
+                else if (instruccion.get(1).equals("database")) {
 
                     CreateDatabase temp = new CreateDatabase();
                     temp.createDatabase(instruccion.get(2));
                     PlanEjecucion plan = new PlanEjecucion("createDatabase", instruccion);
                     break;
 
-                } else if (instruccion.get(1).equals("table")) { 
+                } else if (instruccion.get(1).equals("table")) {
 
                     CreateTable createTab = new CreateTable();
                     int i = 5;
                     Field campo = null;
                     ArrayList<Field> columnas = new ArrayList<>();
-                    
+
                     while (!instruccion.get(i).equals("primary")) {
 
                         if (i + 3 <= instruccion.size()) {
@@ -67,19 +78,19 @@ public class MightyMain {
 
                                     if (i == 5) {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), true, instruccion.get(1), databaseName, true);
-                                        i = i+3;
+                                        i = i + 3;
                                     } else {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), true, instruccion.get(1), databaseName, false);
-                                        i= i+3; 
+                                        i = i + 3;
                                     }
                                 } else {
 
                                     if (i == 5) {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), false, instruccion.get(1), databaseName, true);
-                                        i=i+4;
+                                        i = i + 4;
                                     } else {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), false, instruccion.get(1), databaseName, false);
-                                        i = i+4;
+                                        i = i + 4;
                                     }
                                 }
                             } else if (instruccion.get(i + 1).equals("decimal")) {
@@ -91,16 +102,16 @@ public class MightyMain {
                                         i = i + 8;
                                     } else {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), true, instruccion.get(1), databaseName, false);
-                                        i = i+8;
+                                        i = i + 8;
                                     }
                                 } else {
 
                                     if (i == 5) {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), false, instruccion.get(1), databaseName, true);
-                                        i = i +9;
+                                        i = i + 9;
                                     } else {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), false, instruccion.get(1), databaseName, false);
-                                        i= i+9;
+                                        i = i + 9;
                                     }
                                 }
                             } else {
@@ -109,21 +120,21 @@ public class MightyMain {
 
                                     if (i == 5) {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), true, instruccion.get(1), databaseName, true);
-                                        i = i+6;
+                                        i = i + 6;
                                     } else {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), true, instruccion.get(1), databaseName, false);
-                                        i = i+6;
+                                        i = i + 6;
                                     }
                                 } else {
 
                                     if (i == 5) {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), false, instruccion.get(1), databaseName, true);
-                                        i = i+7;
+                                        i = i + 7;
                                     } else {
                                         campo = new Field(instruccion.get(i), instruccion.get(i + 1), false, instruccion.get(1), databaseName, false);
-                                        i = i+ 7;
+                                        i = i + 7;
                                     }
-                                }                                
+                                }
                             }
                         }
                         columnas.add(campo);
@@ -133,15 +144,14 @@ public class MightyMain {
                     PlanEjecucion plan = new PlanEjecucion("createTable", instruccion);
                     break;
 
-                } else {
-
-                }
+                } 
             case "drop":
 
                 if (instruccion.get(1).equals("table")) {
                     DropTable dropTab = new DropTable();
                     dropTab.dropTable(databaseName, instruccion.get(2));
                     PlanEjecucion plan = new PlanEjecucion("dropTable", instruccion);
+                    plan.procesar();
                     break;
 
                 } else {
@@ -168,6 +178,9 @@ public class MightyMain {
                 break;
 
             case "set":
+                this.databaseName = instruccion.get(2);
+                PlanEjecucion planSD = new PlanEjecucion("set", instruccion);
+                break;
 
             case "alter":
                 AlterTable alter = new AlterTable();
@@ -180,10 +193,45 @@ public class MightyMain {
 
             case "update":
 
+                if (instruccion.size() > 6) {
+                    //   Update update = new Update(instruccion.get(3), instruccion.get(1), instruccion.get(5), instruccion.get(6), 
+                    //         instruccion.get(7), instruccion.get(8) ) ;
+                } else {
+
+                }
             case "delete":
 
+                if (instruccion.size() > 3) {
+
+                    //  Delete del = new Delete(, null, databaseName, instruction0, databaseName, value)
+                }
+
             case "insert":
+
+                ArrayList<String> cols = new ArrayList<>();
+                ArrayList<String> valores = new ArrayList<>();
+
+                int i;
+                for (i = 4; instruccion.get(i).equals(")"); i++) {
+
+                    String elementoActual = instruccion.get(i);
+                    if (!elementoActual.equals(",") && !elementoActual.equals(")")) {
+
+                        cols.add(elementoActual);
+                    }
+                }
+                for (i = i + 2; instruccion.get(i).equals(")"); i++) {
+
+                    String elementoActual = instruccion.get(i);
+                    if (!elementoActual.equals(",") && !elementoActual.equals(")")) {
+
+                        valores.add(elementoActual);
+                    }
+                }
+                InsertInto ins = new InsertInto();
+                ins.executeInsertion(instruccion.get(2), cols, valores, databaseName);
         }
     }
-
 }
+
+
