128644ddcd09f02ab23c01c2578c611a4ffb8ea8
kevuo <kevgiso@hotmail.com>
2015-09-28 04:40:59 -0600

    Método InsertInto finalizado.

--- src/DatabaseRuntimeProcessor/InsertInto.java
+++ src/DatabaseRuntimeProcessor/InsertInto.java
@@ -1,4 +1,5 @@
 package DatabaseRuntimeProcessor;
+
 /**
  * Clase creada con el objetivo de implementar el comando DML insert into. Se
  * recibe como parámetros el nombre de la tabla, un arraylist de las columnas a
@@ -7,60 +8,90 @@ package DatabaseRuntimeProcessor;
  *
  * @author Kevin
  */
-import Shared.Structures.Table;
 import Shared.Structures.Row;
 import Shared.Structures.Field;
+import Shared.Structures.Metadata;
 import java.util.ArrayList;
-import SystemCatalog.FetchMetadata;
 import StoredDataManager.Main.StoredDataManager;
-
+import SystemCatalog.Constants;
 
 public class InsertInto {
 
-    FetchMetadata metadata;
-    StoredDataManager insertManager;
+    private StoredDataManager insertManager;
+    Metadata metadata = new Metadata();
+
     public void executeInsertion(String tableName, ArrayList<String> columns, ArrayList<String> values, String schemaName) {
         boolean doesExist;
-        Row rowToInsert= new Row();
-        doesExist = verifyExistence(schemaName, tableName);
+        insertManager = new StoredDataManager();
+
+        Row rowToInsert = new Row();
+        doesExist = verifyExistence(schemaName, tableName, columns.get(0));
         boolean inOrder;
-        if (doesExist){
-           rowToInsert= buildRow(columns, values, tableName, schemaName);
-           insertManager.insertIntoTable(rowToInsert);
-        }else{
+        if (doesExist) {
+            rowToInsert = buildRow(columns, values, tableName, schemaName);
+            insertManager.insertIntoTable(rowToInsert);
+        } else {
             System.out.println("Operation can not be completed. Table does not exist.");
         }
-        
     }
 
-    private boolean verifyExistence(String schemaName, String tableName) {
-        Table metadataTables = metadata.fetchTables();
-        int length = metadataTables.getLength();
-        Field schemaCol;
-        Field tableCol;
+    private boolean verifyExistence(String schemaName, String tableName, String primaryKey) {
+        metadata = insertManager.deserealizateMetadata();
+        ArrayList<ArrayList<ArrayList<String>>> met = metadata.getMetadata();
+        ArrayList<ArrayList<String>> metadataTables = met.get(Constants.TABLES);
+        int length = metadataTables.size();
+        ArrayList<String> row;
+        String schemaCol;
+        String tableCol;
         boolean result = false;
+        boolean verifyPrimary;
+
         for (int i = 1; i < length; i++) {
-            schemaCol = metadataTables.getRows().get(i).getColumns().get(0);
-            tableCol = metadataTables.getRows().get(i).getColumns().get(1);
-            if (schemaCol.getContent().equals(schemaName) && tableCol.getContent().equals(tableName)) {
-                result = true;
-                break;
+            row = metadataTables.get(i);
+            schemaCol = row.get(Constants.TABLE_SCHNAME);
+            tableCol = row.get(Constants.TABLE_TABNAME);
+            if (schemaCol.equals(schemaName) && tableCol.equals(tableName)) {
+                verifyPrimary = verifyPK(primaryKey, row);
+                if (verifyPrimary) {
+                    result = true;
+                    break;
+                } else {
+                    result = false;
+                    System.out.println("Error. La llave primaria no corresponde con la primera columna de lo ingrsado.");
+                    break;
+                }
+
             } else {
                 result = false;
             }
         }
         return result;
     }
+
     //private boolean elementsInOrder
-    private Row buildRow(ArrayList<String>columns, ArrayList<String> values, String tableName, String schemaName){
+    private boolean verifyPK(String primaryKey, ArrayList<String> row) {
+        String currPK = row.get(Constants.TABLE_PK);
+        return currPK.equals(primaryKey);
+    }
+
+    private Row buildRow(ArrayList<String> columns, ArrayList<String> values, String tableName, String schemaName) {
         Row newTuple = new Row();
         ArrayList<Field> temp = new ArrayList<Field>();
         Field element = new Field();
-        for(int i=0;i<columns.size();i++){
-            element.setContent(values.get(i));
-            element.setSchemaName(schemaName);
-            element.setTableName(tableName);
-            temp.add(element);
+        for (int i = 0; i < columns.size(); i++) {
+            if (i == 0) {
+                element.setContent(values.get(i));
+                element.setSchemaName(schemaName);
+                element.setTableName(tableName);
+                element.setPrimaryKey(true);
+                temp.add(element);
+            } else {
+                element.setContent(values.get(i));
+                element.setSchemaName(schemaName);
+                element.setTableName(tableName);
+                element.setPrimaryKey(false);
+                temp.add(element);
+            }
         }
         newTuple.setColumns(temp);
         return newTuple;
